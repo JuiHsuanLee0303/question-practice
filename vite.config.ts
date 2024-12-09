81f352290920e926bfeaf4037d475084e33abd90
JuiHsuanLee0303 <juihsuanlee0303@gmail.com>
2024-12-09 14:47:23 +0800

    feat: 增加部署到GitHub Page相關設定

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,14 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  base: process.env.NODE_ENV === 'production' ? '/question-practice/' : '/',
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
